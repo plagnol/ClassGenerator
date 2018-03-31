5d146b5e2f67a43523775ca5cee0d411f04ffa46
Antoine Plagnol <32707818+plagnol@users.noreply.github.com>
2018-03-31 22:39:50 +0200

    Initial commit

---
+++ WindowsFormsApp5/Attribut.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classGenerator
+{   
+    /**
+     * 
+     * Attribut of a class
+     * 
+     **/
+    class Attribut
+    {
+        //Name of the attribut
+        private string name;
+
+        //Type of the attribut
+        private string type;
+
+        /**
+         * 
+         * Constructor of the class
+         * 2 params : 
+         * string @nameSend
+         * string @typeSend
+         * 
+         * set the name and the type of the attribut
+         * 
+         * return void
+         **/
+        public Attribut(string nameSend, string typeSend)
+        {
+            this.setName(nameSend);
+            this.setType(typeSend);
+        }
+        
+        /**
+         * Get the name of the attribut
+         * return
+         * string @this.name
+         **/
+        public string getName()
+        {
+            return this.name;
+        }
+
+        /**
+         * Get the type of the attribut
+         * return
+         * string @this.type
+         **/
+        public string getType()
+        {
+            return this.type;
+        }
+        /**
+         * Set the name of the attribut
+         * param 
+         * string @nameSend 
+         **/
+        public void setName(string nameSend)
+        {
+            this.name = nameSend;
+        }
+
+        /**
+         * Set the type of the attribut
+         * param 
+         * string @typeSend 
+         **/
+        public void setType(string typeSend)
+        {
+            this.type = typeSend;
+        }
+    }
+}
---
+++ WindowsFormsApp5/ClassManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classGenerator
+{
+    class ClassManager
+    {
+        //List of attribut of the class
+        List<Attribut> attributs;
+        
+        /**
+         *Constructor of the class; 
+         **/
+        public ClassManager()
+        {
+            this.attributs = new List<Attribut>();
+        }
+
+        
+
+        
+    }
+}
